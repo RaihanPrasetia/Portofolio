03c3aa8108f811367f0329f246802ace3bdaa17e
RaihanPrasetia <133187330+RaihanPrasetia@users.noreply.github.com>
2024-10-14 12:50:58 +0700

    menambahakn project

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,19 @@
 
     document.addEventListener('DOMContentLoaded', function () {
-        // Mengambil semua link pada navigasi
         const navLinks = document.querySelectorAll('.nav-link');
         const sections = document.querySelectorAll('section');
 
         const observerOptions = {
-            threshold: 0.5 // Mendeteksi saat 50% dari section terlihat
+            threshold: 0.5 
         };
 
         const observer = new IntersectionObserver((entries) => {
             entries.forEach(entry => {
                 if (entry.isIntersecting) {
-                    // Menghilangkan class active dari semua link
                     navLinks.forEach(link => {
                         link.classList.remove('active');
                     });
 
-                    // Menambahkan class active ke link yang sesuai
                     const activeLink = document.querySelector(`.nav-link[href="#${entry.target.id}"]`);
                     if (activeLink) {
                         activeLink.classList.add('active');
@@ -25,17 +22,33 @@
             });
         }, observerOptions);
 
-        // Observasi setiap section
         sections.forEach(section => {
             observer.observe(section);
         });
 
-        // Tambahkan class active ketika link di klik
         navLinks.forEach(link => {
             link.addEventListener('click', function () {
-                navLinks.forEach(l => l.classList.remove('active')); // Hilangkan class active dari semua link
-                this.classList.add('active'); // Tambahkan class active ke link yang diklik
+                navLinks.forEach(l => l.classList.remove('active')); 
+                this.classList.add('active'); 
             });
+        });
+
+        const carousel = document.querySelector('#skillsCarousel');
+        carousel.addEventListener('slide.bs.carousel', function (event) {
+            const activeSlide = event.relatedTarget;
+            const cards = activeSlide.querySelectorAll('.card');
+
+            cards.forEach((card, index) => {
+                card.style.animationDelay = `${index * 0.3}s`;
+                card.classList.add('card-animate');
+            });
+        });
+
+        const initialActiveSlide = carousel.querySelector('.carousel-item.active');
+        const initialCards = initialActiveSlide.querySelectorAll('.card');
+        initialCards.forEach((card, index) => {
+            card.style.animationDelay = `${index * 0.3}s`;
+            card.classList.add('card-animate');
         });
     });
 
